8ec02aeab02e1daf8862ee66fcab5ab948fb8b0d
uustop <yth3412@163.com>
2018-01-19 17:08:22 +0800

    rest test

---
+++ src/main/java/com/uustop/sblearning/SblearningApplication.java
@@ -0,0 +1,12 @@
+package com.uustop.sblearning;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SblearningApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SblearningApplication.class, args);
+	}
+}
