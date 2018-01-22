b6dd5a78a101f29f0c5e30a6b92b3144bdbc7cf4
uustop <yth3412@163.com>
2018-01-22 11:21:32 +0800

    exception

--- src/main/java/com/uustop/sblearning/HelloController.java
+++ src/main/java/com/uustop/sblearning/HelloController.java
@@ -1,14 +1,42 @@
 package com.uustop.sblearning;
 
+import com.uustop.sblearning.sbl1.MyException;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.ModelMap;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
 import org.springframework.web.bind.annotation.RestController;
 
-@RestController
+/**
+
+ *
+
+ * @author 程序猿DD
+
+ * @version 1.0.0
+
+ * @blog http://blog.didispace.com
+
+ *
+
+ */
+@Controller
 public class HelloController {
 
-
     @RequestMapping("/hello")
-    public String index() {
-        return "Hello World";
+    public String hello() throws Exception {
+        throw new Exception("发生错误");
     }
+
+    @RequestMapping("/")
+    public String index(ModelMap map) {
+        map.addAttribute("host", "http://www.uustop.com");
+        return "index";
+    }
+
+    @RequestMapping("/json")
+    public String json() throws MyException {
+        throw new MyException("发生错误2");
+    }
+
 }
