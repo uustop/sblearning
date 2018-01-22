1706dbc013f398318b7b27cefc402365f231b4f1
uustop <yth3412@163.com>
2018-01-22 15:49:45 +0800

    Spring-data-jpa

--- src/test/java/com/uustop/sblearning/dbTests.java
+++ src/test/java/com/uustop/sblearning/dbTests.java
@@ -2,6 +2,8 @@ package com.uustop.sblearning;
 
 
 import com.uustop.sblearning.data.UserService;
+import com.uustop.sblearning.db2.domain.User;
+import com.uustop.sblearning.db2.service.UserRepository;
 import org.junit.Assert;
 import org.junit.Before;
 import org.junit.Test;
@@ -40,6 +42,44 @@ public class dbTests {
 
         // 查数据库，应该有5个用户
         Assert.assertEquals(3, userSerivce.getAllUsers().intValue());
+
+    }
+
+    @Autowired
+    private UserRepository userRepository;
+
+    @Test
+    public void testdb1() throws Exception {
+
+        // 创建10条记录
+        userRepository.save(new User("AAA", 10));
+        userRepository.save(new User("BBB", 20));
+        userRepository.save(new User("CCC", 30));
+        userRepository.save(new User("DDD", 40));
+        userRepository.save(new User("EEE", 50));
+        userRepository.save(new User("FFF", 60));
+        userRepository.save(new User("GGG", 70));
+        userRepository.save(new User("HHH", 80));
+        userRepository.save(new User("III", 90));
+        userRepository.save(new User("JJJ", 100));
+
+        // 测试findAll, 查询所有记录
+        Assert.assertEquals(10, userRepository.findAll().size());
+
+        // 测试findByName, 查询姓名为FFF的User
+        Assert.assertEquals(60, userRepository.findByName("FFF").getAge().longValue());
+
+        // 测试findUser, 查询姓名为FFF的User
+        Assert.assertEquals(60, userRepository.findUser("FFF").getAge().longValue());
+
+        // 测试findByNameAndAge, 查询姓名为FFF并且年龄为60的User
+        Assert.assertEquals("FFF", userRepository.findByNameAndAge("FFF", 60).getName());
+
+        // 测试删除姓名为AAA的User
+        userRepository.delete(userRepository.findByName("AAA"));
+
+        // 测试findAll, 查询所有记录, 验证上面的删除是否成功
+        Assert.assertEquals(9, userRepository.findAll().size());
 
     }
 
